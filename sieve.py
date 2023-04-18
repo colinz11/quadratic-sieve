f8fc38b2b23a1c381b99c7d9e8a3083d1e77bf61
Keith Cressman <keith.cressman@duke.edu>
2023-04-17 20:14:31 -0400

    fixed problems with float vs int

--- sieve.py
+++ sieve.py
@@ -1,5 +1,6 @@
-from math import pow, sqrt, exp, log, ceil
+from math import sqrt, exp, log, ceil
 from itertools import chain
+import time
 
 #1. generate B-smooth factor base
 #2. Find B-smooth relations using sieving and Tonelli-Shanks
@@ -21,6 +22,14 @@ def gcd(a,b):
         b %= a
     return 1
 
+#compute a^n
+def int_pow(a, n):
+    result = 1
+    for i in range(n):
+        result *= a
+    return result
+
+
 
 #returns a^n mod p FINISHED
 def power(a, n, p):
@@ -37,8 +46,9 @@ def power(a, n, p):
 def legendre(a, p):
     return power(a, (p - 1) // 2, p)
 
+
 #generate primes up until B and use euler’s criterion to determine whether N is a quadratic residue mod p FINISHED
-def find_factor_base(N, B):
+def find_factor_base(N, B, epsilon = 0.0000001):
     if B < 2:
         return []
     
@@ -48,8 +58,8 @@ def find_factor_base(N, B):
     factor_base = []
     while p < B + 1: 
         if is_prime[p]:
-            if legendre(N, p) == 1: #finds our factor base
-                factor_base.append(p)
+            if legendre(N,p) == 1:
+                    factor_base.append(p)
             for j in range(2 * p, B + 1, p):
                 is_prime[j] = False
         p += 1
@@ -57,49 +67,54 @@ def find_factor_base(N, B):
    
 #picks optimal bound B FINISHED
 def get_smoothness_bound(N):
-    epsilon = 0
+    epsilon = 0.1
     e = 2.71
     B = e ** ((0.5 + epsilon) * sqrt(ceil(log(N)*log(log(N)))))
     return ceil(B)
 
 #does (x + n)^2 - N where x = sqrt(N) to find B-smooth numbers. Then use Tonelli and Shanks to compute resiudes for each prime in factor base
-def find_smooth(factor_base, N, interval, tolerance=1):
+def find_smooth(factor_base, N, interval, tolerance=3):
     root = ceil(sqrt(N))
     x = [] 
     smooth_nums = []
+    factors = {}
     for i in range(interval):
-
-        sieve_list = int(pow(root + i, 2) - N)
-        sieve_static = [sieve_list][0]
-
-        for p in factor_base: #skip 2
-            while sieve_list % p == 0:#p is a prime factor
-                sieve_list //= p #divide by prime in factor base
+        sieve_num = int(int_pow(root + i, 2) - N)
+        sieve_static = [sieve_num][0]
+        prime_factors = []
+        for p in factor_base: 
+            while sieve_num % p == 0:#p is a prime factor
+                sieve_num //= p #divide by prime in factor base
+                prime_factors.append(p)
 
         
-
-        if sieve_list == 1:
+        if sieve_num == 1:
             smooth_nums.append(sieve_static)
+            factors[sieve_static] = prime_factors
             x.append(i+root) #x+n
-            print('Smooth Number: '+str(sieve_static))
-        
-        if len(smooth_nums) >= len(factor_base) + tolerance: 
+            if (len(smooth_nums) % 10 == 0):
+                print(str(time.ctime()) + 'Smooth Number ' + str(len(smooth_nums))  + ':'+ str(sieve_static))
+        if len(smooth_nums) >= (len(factor_base) * tolerance) + 1: 
               break
-    return smooth_nums, x
+    return smooth_nums, x, factors
 
 #builds matrix of exponents of prime factors of smooth numbers mod 2
-def build_matrix(factor_base, smooth_nums):
-    matrix = []
+def build_matrix(factor_base, smooth_nums, factors):
+    matrix = [] #binary version
+    matrix2 = [] #non-binary version
 
     for n in smooth_nums:
         exp_vector = [0]*(len(factor_base))
-        prime_factors = factor(n, factor_base)
+        exp_vector2 = [0]*(len(factor_base))
+        prime_factors = factors[n]
 
         for i in range(len(factor_base)):
             if factor_base[i] in prime_factors:
                 exp_vector[i] = (exp_vector[i] + prime_factors.count(factor_base[i])) % 2 #get exponenet vector of prime factors mod 2
+                exp_vector2[i] = prime_factors.count(factor_base[i])
         matrix.append(exp_vector)
-    return matrix
+        matrix2.append(exp_vector2)
+    return matrix, matrix2
 
 def factor(n, factor_base):#trial division from factor base
     factors = []
@@ -162,21 +177,36 @@ def solve_row(matrix, marks, solution_rows, K):
     return solution_vector
 
 #caulates a^2 = b^2 mod n and does gcd(a+b, n)
-def solve(N, solution_vector, smooth_nums, x):
+def solve(N, solution_vector, smooth_nums, x, factor_base, matrix2):
     nums = [smooth_nums[i] for i in solution_vector]
     x_nums = [x[i] for i in solution_vector]
 
+    for i in range(len(x_nums)):
+        if ((x_nums[i] ** 2 % N) != nums[i] ):
+            print("wtf, " + str(x_nums[i]) + " squared mod N not equal to " + str(nums[i]))
+
+
     a = 1
     b = 1
+    smooth_vector = [0 for i in factor_base] #this is an exponent vector
 
-    for n in nums:
-        a *= n
-        a = a % N
+
+    for i in solution_vector:
+        for j in range(len(factor_base)):
+            smooth_vector[j] += matrix2[i][j]
+
+    for i in range(len(smooth_vector)):
+        f = 1
+        for j in range(smooth_vector[i] >> 1):
+            f = f * (factor_base[i])
+
+        a *= f
+
     for n in x_nums:
         b *= n
-            
-    a = sqrt(a)
 
+
+    
     return gcd(a+b, N)    
 
 
@@ -234,34 +264,46 @@ def tonelli_shanks(n, p):
     return r, p-r
 
 def main():
-    N = 97*1009
+    #N = 1009 * 191161
+    #N = 4201 * 8011
+    #N = 484459 * 191161
+    #N = 4201 * 484459
+    #N = 1000033 * 1000003
+    #N = 10000019 * 10000169
+    #N = 100000007 * 10000169
+    #N = 100000007 * 100000049
+    N = 16921456439215439701
+    #N = 46839566299936919234246726809
+    #N = 6172835808641975203638304919691358469663
+
     B = get_smoothness_bound(N)
 
     print('Bound: ' + str(B))
     
     factor_base = find_factor_base(N, B)
+    print("num primes in factor base is " + str(len(factor_base)))
 
     
     print('Finding smooth numbers ...')
-    smooth_nums, x = find_smooth(factor_base, N, B**3)
+    smooth_nums, x, factors = find_smooth(factor_base, N, B**3, tolerance=1.01)
 
     if len(smooth_nums) < len(factor_base):
         print('No smooth numbers')
         return
     
     print('Found smooth numbers')
-    matrix = build_matrix(factor_base, smooth_nums)
-
+    #below, matrix is all mod 2 entries, while matrix2 has any positive integer for each entry
+    matrix, matrix2 = build_matrix(factor_base, smooth_nums, factors) 
 
     print('Running guass elimination ... ')
     solution_rows, marks, matrix = gauss_elimination(matrix)
 
 
-    
+
     for K in range(len(solution_rows)):
         print('Finding factors ...')
         solution_vector = solve_row(matrix, marks, solution_rows, K)
-        factor = solve(N, solution_vector, smooth_nums, x)
+        factor = solve(N, solution_vector, smooth_nums, x, factor_base, matrix2)
         if factor == 1 or factor == N:
             print(factor)
             print("Trivial factor")
