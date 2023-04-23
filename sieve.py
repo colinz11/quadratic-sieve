5985377cf704c94e64c12788a17ce5aaea88a746
Keith Cressman <keith.cressman@duke.edu>
2023-04-23 12:42:47 -0400

    implemented tonelli shanks

--- sieve.py
+++ sieve.py
@@ -109,6 +109,136 @@ def find_smooth_numbers(factor_base, N, interval, extra_rows=1):
               break
     return smooth_nums, x, factors
 
+def lower_register(orig, p):
+    while (orig % p) == 0:
+        orig //= p
+    return orig
+
+def lower_register_log(orig, log_p):
+    return orig - log_p
+
+def check_smooth(f, x, N, factor_base, smooth_nums, x_list, factors):
+    if f < 1:
+        return
+    if f == 1:
+        #f(x) is smooth, where f(x) = (x)^2 - N
+        x_list.append(x)
+        f_orig = x * x - N
+        smooth_nums.append(f_orig)
+        if (len(smooth_nums) % 20 == 0):
+            print("smooth nums: " + str(len(smooth_nums)))
+        #now find factors of f_orig
+        prime_factors = []
+        for p in factor_base:
+            while f_orig % p == 0:
+                f_orig //= p
+                prime_factors.append(p)
+        factors[x * x - N] = prime_factors
+
+def check_smooth_log(f, x, N, factor_base, smooth_nums, x_list, factors, threshold):
+    if f <= 0:
+        return
+    elif f < threshold:
+
+        #f(x) is smooth, where f(x) = (x)^2 - N
+        '''
+        x_list.append(x)
+        f_orig = x * x - N
+        smooth_nums.append(f_orig)
+        if (len(smooth_nums) % 20 == 0):
+            print("smooth nums: " + str(len(smooth_nums)))
+        #now find factors of f_orig
+        prime_factors = []
+        for p in factor_base:
+            while f_orig % p == 0:
+                f_orig //= p
+                prime_factors.append(p)
+        factors[x * x - N] = prime_factors
+        '''
+        prime_factors = []
+        f_orig = x * x - N
+        for p in factor_base:
+            while f_orig % p == 0:
+                f_orig //= p
+                prime_factors.append(p)
+        if (f_orig > 1):
+            return
+        else:
+            if (len(smooth_nums) % 20 == 0):
+                print(str(time.ctime()) + ": " + str(len(smooth_nums)) + " smooths have been found")
+            x_list.append(x)
+            smooth_nums.append(x * x - N)
+            factors[x * x - N] = prime_factors
+        
+
+
+
+
+
+
+def find_smooth_numbers_TS(factor_base, N, interval, extra_rows=1):
+    root = ceil(sqrt(N))
+    x_list = [] 
+    smooth_nums = []
+    factors = {}
+    num_needed = len(factor_base) + extra_rows
+    print("number of smooth numbers needed is " + str(num_needed))
+
+    solutions_mod_p = [TS(i, N) for i in factor_base]
+
+    x_upper_lim = int((sqrt(2) - 1)*sqrt(N) - 1)
+   
+
+    length = min(max(factor_base) * 10, x_upper_lim)
+    log_p = [log(p) for p in factor_base]
+    threshold = log(max(factor_base) + 1)
+    
+    end=-1
+    while len(smooth_nums) < num_needed:
+        start=end+1
+        end = start+length
+
+        #f_x = [(root + start + i) * (root + start + i) - N for i in range(length)]
+        f_x = [log((root + start + i) * (root + start + i) - N) for i in range(length)]
+        for i in range(len(factor_base)):
+            p_i = factor_base[i]
+            (x1, x2) = solutions_mod_p[i]
+            #iterate through the f_x array dividing out p_i 
+            #first_val_1 = (ceil((root + start) / p_i)  * p_i) - start - root + x1 #first val s.t. f_x[first_val_1] divisible by p_i
+            first_val_1 = (0 - ((root + start) // (0 - p_i))  * p_i) - start - root + x1
+            v1 = root + start
+            v2 = 0 -(v1 // (0 - p_i))
+            v3 = ceil(v2)
+            v4 = v3 * p_i
+            v5 = v4 - start - root + x1
+            #first_val_2 = (ceil((root + start) / p_i)  * p_i) - start - root + x2
+            first_val_2 = (0 - ((root + start) // (0 - p_i))  * p_i) - start - root + x2
+            if first_val_1 >= p_i:
+                first_val_1 -= p_i
+            if first_val_2 >= p_i:
+                first_val_2 -= p_i
+
+
+            
+            for x_val in range(first_val_1, length, p_i):
+                #divide out p
+                #f_x[x_val] = lower_register(f_x[x_val], p_i)
+                f_x[x_val] = lower_register_log(f_x[x_val], log_p[i])
+                #check that x_val+start is correct argument
+                #check_smooth(f_x[x_val], x_val+start+root, N, factor_base, smooth_nums, x_list, factors)
+                check_smooth_log(f_x[x_val], x_val+start+root, N, factor_base, smooth_nums, x_list, factors, threshold)
+
+            for x_val in range(first_val_2, length, p_i):
+                #divide out p
+                #f_x[x_val] = lower_register(f_x[x_val], p_i)
+                f_x[x_val] = lower_register_log(f_x[x_val], log_p[i])
+                #check_smooth(f_x[x_val], x_val+start+root, N, factor_base, smooth_nums, x_list, factors)
+                check_smooth_log(f_x[x_val], x_val+start+root, N, factor_base, smooth_nums, x_list, factors, threshold)
+
+
+    return smooth_nums, x_list, factors
+
+
 
 
 #builds matrix of exponents of prime factors of smooth numbers mod 2
@@ -243,62 +373,49 @@ def check_prime(n, iter=10):
             j += 1
     return True
 
-#tonelli algorithim copied from internet, two solutionrs r and p-r
-def tonelli_shanks(n, p): 
+
+#copied from internet
+def TS(p, n):
+
+    if legendre_num(n, p) != 1:
+        print("not a square (mod p)")
     q = p - 1
     s = 0
-
     while q % 2 == 0:
         q //= 2
         s += 1
-
     if s == 1:
-        r = exp_mod(n, (p + 1) // 4, p)
-        return r, p-r
-    
-    for z in range(2, p):
-        if p - 1 == legendre_num(z, p):
+        r = pow(n, (p + 1) // 4, p)
+        r2 = (p - r) % p
+        return (r, r2)
+    z = 2
+    for z_val in range(2, p):
+        z = z_val
+        if p - 1 == legendre_num(z_val, p):
             break
-
-    c = exp_mod(z, q, p)
-    r = exp_mod(n, (q + 1) // 2, p)
-    t = exp_mod(n, q, p)
+    c = pow(z, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
     m = s
     t2 = 0
-
     while (t - 1) % p != 0:
         t2 = (t * t) % p
+        
         for i in range(1, m):
             if (t2 - 1) % p == 0:
                 break
             t2 = (t2 * t2) % p
-        b = exp_mod(c, 1 << (m - i - 1), p)
+        b = pow(c, 1 << (m - i - 1), p)
         r = (r * b) % p
         c = (b * b) % p
         t = (t * c) % p
         m = i
-    return r, p-r
 
-def main(N=21, epsilon=0.1):
+    return (r, (p-r)%p)
+
+def factor(N=21, epsilon=0.001):
     start_time_ms = int(time.time() * 1000)
-    #N = 55587
-    #N = 1009 * 191161
-    #N = 37 * 1009
-    #N = 484459 * 191161
-    #N = 4201 * 484459
-    #N = 1000033 * 1000003
-    #N = 10000019 * 10000169
-    #N = 100000007 * 10000169
-    #N = 100000007 * 100000049
-<<<<<<< HEAD
-    #N = 16921456439215439701
-=======
-    N = 16921456439215439701
->>>>>>> 9fef4a78833dce337ffcfe2eda422ae1982ce2b0
-    #N = 100000007 * 10000169312
 
-    #N = 46839566299936919234246726809
-    #N = 6172835808641975203638304919691358469663
     if check_prime(N):
         print("N is very likely prime")
         return
@@ -311,14 +428,20 @@ def main(N=21, epsilon=0.1):
 
     time_0 = int(time.time() * 1000)
     print('Finding smooth numbers ...')
-    smooth_nums, x, factors = find_smooth_numbers(factor_base, N, B**3, extra_rows=20)
-    found_smooth_time_ms = int(time.time() * 1000) - time_0
+    extra_rows = int(20 + log(N))
+    if (N < ( 10 ** 15 )):
+        smooth_nums, x, factors = find_smooth_numbers(factor_base, N, B**3, extra_rows)
+    else:
+        smooth_nums, x, factors = find_smooth_numbers_TS(factor_base, N, B**3, extra_rows)
+        if smooth_nums == None:
+            smooth_nums, x, factors = find_smooth_numbers(factor_base, N, B**3, extra_rows)
+    found_smooth_time_ms = (time.time() * 1000) - time_0
 
     if len(smooth_nums) < len(factor_base):
         print('Not enough B-smooth numbers were found')
         return
     else:
-        print('Found smooth numbers')
+        print('Found smooth numbers in ' + str(found_smooth_time_ms / 1000) + "s")
 
     #below, matrix is all mod 2 entries, while matrix_nb has any positive integer for each entry
     matrix, matrix_nb = make_matrix(factor_base, smooth_nums, factors) 
@@ -330,24 +453,46 @@ def main(N=21, epsilon=0.1):
 
     null_combos = find_left_null_vectors(matrix)
     elim_time = int(int(time.time() * 1000) - time_1)
+    print("elim time: " + str(elim_time / 1000) + "s")
     num_solutions = len(null_combos)
 
-
     for s in range(num_solutions):
-        print('Finding factors ...')
         [a, b] = find_squared_congruence(null_combos[s], x, factor_base, matrix_nb)
-        factor = abs(gcd(a-b, N))
+        possible_factor = abs(gcd(a-b, N))
 
-        if factor == 1 or factor == N:
+        if possible_factor == 1 or possible_factor == N:
             print("Trivial Factor")
             continue
         else:
-            print("N = " + str(factor) + " * " + str(int(N/factor)))
+            print("N = " + str(possible_factor) + " * " + str(int(N/possible_factor)))
             total_time = int(time.time() * 1000) - start_time_ms
+            print("total time: " + str(total_time) + "ms, finding smooths time: " + str(found_smooth_time_ms) +
+                   "ms, Gaussian elim time: " + str(elim_time) + "ms")
             return [found_smooth_time_ms, elim_time, total_time]
         
     #if the code reaches this point, no nontrivial factors have been found
-    print("Could not find a nontrivial factor")
+    #retry with higher epsilon
+    print("Could not find a nontrivial factor, retrying to factor")
+    return factor(N, epsilon * 10)
 
+def main():
+    #N= 21
+    #N = 4201 * 8011
+    #N = 55587
+    #N = 1009 * 191161
+    #N = 37 * 1009
+    #N = 484459 * 191161
+    #N = 4201 * 484459
+    #N = 1000033 * 1000003
+    #N = 10000019 * 10000169
+    #N = 100000007 * 10000169
+    #N = 100000007 * 100000049
+    #N = 16921456439215439701
+    #N = 46839566299936919234246726809
+    N = 1000000000100011 * 1084051191974761
+    #N = 6172835808641975203638304919691358469663
+    #N = 3744843080529615909019181510330554205500926021947
+    return factor(N, epsilon=0.005)
 if __name__ == "__main__":
-    main()
+    print(main())
+
